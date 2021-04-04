1998d52a38dcd536427fb0d5c63960098528859a
anton-3003 <68605875+anton-3003@users.noreply.github.com>
2021-04-04 13:08:59 +0300

    updated

--- app/serializers.py
+++ app/serializers.py
@@ -4,9 +4,26 @@ from django.contrib.auth.models import User
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    # profile = ProfileSerializer()
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name']
+
+    # def create(self, validated_data):
+    #     return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    # posts = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,16 +34,11 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     # def create(self, validated_data):
-    #     as_data = validated_data.pop('author')
+    #     authors_data = validated_data.pop('author')
     #     post = Post.objects.create(**validated_data)
-    #     for a_data in as_data:
-    #         User.objects.create(post=post, **a_data)
+    #     for a_data in authors_data:
+    #         Post.objects.create(post=post, **a_data)
     #     return post
+
     def create(self, validated_data):
         return Post.objects.create(**validated_data)
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from .models import Post, Category
 from .serializers import PostSerializer, CategorySerializer
-from rest_framework import generics
+from rest_framework import generics, serializers
+from requests import Response
 
 
 # class PostList(generics.ListAPIView):
@@ -18,7 +19,23 @@ class CatList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def get_fields(self):
+        fields = super(CategorySerializer, self).get_fields()
+        fields['post'] = CategorySerializer(many=True)
+        return fields
+
+    # def get(self, request):
+    #     post = Post.objects.all()
+    #     serializer = PostSerializer(post, many=True)
+    #     return Response({'post': serializer.data})
+
 
 class CatDetail(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_fields(self):
+        fields = super(CategorySerializer, self).get_fields()
+        fields['post'] = CategorySerializer(many=True)
+        return fields
+
